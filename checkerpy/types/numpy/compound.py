1ef93c2ee3aa89592ed9286f9d0426df7e48c22d
Georg Heimel <georg.heimel@snapp.cab>
2018-03-14 16:25:26 +0100

    Updated JustNpNum

--- checkerpy/types/numpy/compound.py
+++ checkerpy/types/numpy/compound.py
@@ -1,5 +1,4 @@
-from numpy import int16, int32, int64, float16, float32, float64
+from numpy import int32, int64, float32, float64
 from .justdtype import JustDtype
 
-JustNpNum = JustDtype(int16, int32, int64, float16, float32, float64,
-                      identifier='JustNpNum')
+JustNpNum = JustDtype(int32, int64, float32, float64, identifier='JustNpNum')
